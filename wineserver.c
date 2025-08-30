f4b6db918d0f69a36d2f42c3ddfeb054e382a61f
stakeval0 <stakeval0@gmail.com>
2025-08-31 07:30:59 +0900

    improve: wineserver.c with relative path

--- wineserver.c
+++ wineserver.c
@@ -2,16 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <libgen.h>
+#include <limits.h>
 #include <fcntl.h>
 #include <elf.h>
 #include <errno.h>
 
-#define TARGET_PATH "/opt/wine/bin/wineserver.real"
+#define PATH_MAX 4096
 #define BOX86_PATH "box86"
 #define BOX64_PATH "box64"
 
 int main(int argc, char *argv[]) {
-    int fd = open(TARGET_PATH, O_RDONLY);
+    char exe_path[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", exe_path, sizeof(exe_path) - 1);
+    if (len == -1) {
+        perror("readlink");
+        return 1;
+    }
+    exe_path[len] = '\0';
+
+    // Get directory path of current binary
+    char *dir = strdup(exe_path);
+    if (!dir) {
+        perror("strdup");
+        return 1;
+    }
+
+    char *base = basename(exe_path);
+    char *dir_path = dirname(dir);
+
+    // Construct wineserver.real path
+    char real_path[PATH_MAX];
+    snprintf(real_path, sizeof(real_path), "%s/%s.real", dir_path, base);
+    free(dir);
+
+    // Open ELF header
+    int fd = open(real_path, O_RDONLY);
     if (fd < 0) {
         perror("open");
         return 1;
@@ -25,14 +51,12 @@ int main(int argc, char *argv[]) {
     }
     close(fd);
 
-    // Check ELF magic
     if (memcmp(e_ident, ELFMAG, SELFMAG) != 0) {
-        fprintf(stderr, "Not an ELF file: %s\n", TARGET_PATH);
+        fprintf(stderr, "Not an ELF file: %s\n", real_path);
         return 1;
     }
 
     const char *loader = NULL;
-
     switch (e_ident[EI_CLASS]) {
         case ELFCLASS32:
             loader = BOX86_PATH;
@@ -45,7 +69,7 @@ int main(int argc, char *argv[]) {
             return 1;
     }
 
-    // execvp: loader, [loader, wineserver.real, ...]
+    // Build new argv list: loader real_path args...
     char **new_argv = malloc(sizeof(char *) * (argc + 2));
     if (!new_argv) {
         perror("malloc");
@@ -53,7 +77,7 @@ int main(int argc, char *argv[]) {
     }
 
     new_argv[0] = (char *)loader;
-    new_argv[1] = (char *)TARGET_PATH;
+    new_argv[1] = real_path;
     for (int i = 1; i < argc; ++i) {
         new_argv[i + 1] = argv[i];
     }
